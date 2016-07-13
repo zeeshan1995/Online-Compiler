52db7ecc7b7bd45b4e590baa32f543cce1a963b1
Zeeshan Akhter <zeeshanakhter1729@gmail.com>
2016-07-13 20:55:18 +0530

    Running Feature Added

--- server.js
+++ server.js
@@ -2,51 +2,68 @@ var express=require('express');
 var app=express();
 var server=require('http').Server(app);
 var fs=require('fs');
-var body_parser=require('body-parser');
 var sys=require('util');
 var exec=require('child_process').exec;
 var io=require('socket.io')(server);
 var uid=require('uid');
-app.use(body_parser.json());
-app.use(body_parser.urlencoded({extended:true}));
+
 
 server.listen(8000,function(){console.log('Server is up.');});
 
-app.get('/',function(req,res){
+app.get('/*',function(req,res){
     res.sendFile(__dirname + '/index.html');
 })
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(find, 'g'), replace);
+}
+
 var codes={};
 io.on('connection',function(socket){
     
-    
-    socket.on('new_room',function(a){
-        var x=uid();
-        socket.room=x;
-        socket.join(x);
-        socket.emit('room_name',x);
-    });
-    
-    socket.on('join_room',function(a){
-        socket.room=a;
-        socket.join(a);
-        socket.emit('room_name',a);
+    if((socket.handshake.headers.referer=="http://192.168.1.6:8000/")||( socket.handshake.headers.referer=="http://localhost:8000/")){
+        var room=uid();
+        socket.room=room;
+        socket.join(room);
+        socket.emit('room_name',room);
+    }
+    else{
+        var room=socket.handshake.headers.referer.split('/')[3];
+        socket.room=room;
+        socket.join(room);
         socket.emit('edited',codes[socket.room]);
-     //   socket.to(socket.room).emit('edited',codes[socket.room]);//Send only to sender in the room
-    });
-    
-    socket.on('compile',function(code){
-        fs.writeFile(__dirname+"/" + socket.room + ".cpp",code,function(err){
+    } 
+    socket.on('compile',function(code,input){
+        fs.writeFile(__dirname+"/codes/" + socket.room + ".cpp",code,function(err){
             if(err){
                 return console.log("Could not write");
             }
             else{
                 var msg;
-                exec("g++ "+socket.room+".cpp -o " + socket.room,function(error,stdout,stderr){
+                exec("g++ codes/"+socket.room+".cpp -o " + "codes/" + socket.room,function(error,stdout,stderr){
                     msg=stderr;
-                    msg=msg.replace('\n',"<br><br>");
-                    if(!msg)
-                        msg="Compilation Successfull."
-                    io.in(socket.room).emit('compile_msg' , msg);//Send to all in the room including the sender
+                    msg=replaceAll(msg,'\n',"<br>");
+                    if(!msg){
+                        fs.writeFile(__dirname+"/codes/" + socket.room + ".txt",input,function(err){
+                            if(err){
+                                return console.log("Could not write");
+                            }
+                            else{
+                              exec("./codes/"+socket.room+" < codes/" + socket.room + ".txt",function(error,stdout,stderr){
+                                  if(stderr)
+                                      msg=stderr;
+                                  else if(stdout)
+                                      msg=stdout;
+                                  else
+                                      msg="Compilation Successfull."
+                                  msg=replaceAll(msg,'\n','<br>');
+                                  io.in(socket.room).emit('compile_msg',msg);
+                              });   
+                            }
+                        });
+                    }
+                    else
+                        io.in(socket.room).emit('compile_msg' , msg);//Send to all in the room including the sender
                 });
             }
         });
@@ -57,30 +74,3 @@ io.on('connection',function(socket){
         socket.broadcast.to(socket.room).emit('edited',code);//Send to all in the room except the sender.
     })
 });
-
-
-
-
-/*
-app.post('/compile',function(req,res){
-    fs.writeFile(__dirname+"/code.cpp",req.body.code,function(err){
-        if(err){
-            return console.log("Could not write");
-        }
-        else{
-            var msg;
-            exec("g++ code.cpp",function(error,stdout,stderr){
-                console.log("Compiling");
-                msg=stderr;
-                if(msg)
-                    console.log("Not empty error.");//\nError::\n"+ msg)
-                else{
-                    console.log("compilation Successfull");
-                    msg="Compilation Successfull."
-                }
-                console.log("msg::"+msg);
-                res.send(msg);
-            });
-        }
-    });
-});*/
