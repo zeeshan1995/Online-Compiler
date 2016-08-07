1dbb918904e66f29404e8a5e2d5d3b8c53666fed
Zeeshan Akhter <zeeshanakhter1729@gmail.com>
2016-08-07 12:23:07 +0530

    Refactoring

--- cache/73p6n18/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <stdio.h>
-int main()
-{
-char * s;
-scanf("%s",&s);
-//std::cout<<"Hello World!\n";
-}
--- cache/9us0agd/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-char * s;
-scanf("%s",&s);
-//std::cout<<"Hello World!\n";
-}
